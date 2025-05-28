c546ecb7cfc233832245f4d9e4bed82faa45f220
AnanyaBinduN <160011712+AnanyaBinduN@users.noreply.github.com>
2025-05-28 11:17:30 +0530

    Add files via upload

---
+++ pm10-main/pm8-main/gsss/src/main/java/com/Aishwarya157-creator/App.java
@@ -0,0 +1,13 @@
+package com.Aishwarya157-creator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
